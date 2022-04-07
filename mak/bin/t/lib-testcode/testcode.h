cfb41aa453a72895aa4f3207cc634c2812abda7e
Jim Belton <jim.belton@gmail.com>
2022-04-06 17:18:10 -0700

    Use the mak submodule

--- mak/bin/t/lib-testcode/testcode.h
+++
@@ -1,9 +0,0 @@
-/* More test code for genxface.pl
- */
-
-#ifndef TESTCODE_H_INCLUDED
-
-#include "lib-testcode-proto.h"
-inline int inlines_need_prototypes(void * why_i_dont_know) {return (int)why_i_dont_know;}
-
-#endif
--- mak/mak-common-shell.mak
+++
@@ -1,7 +0,0 @@
-shell:
-ifeq ($(OS_class),any-winnt)
-	@cmd.exe
-else
-	@$(REMOTE_SHELL)
-endif
-
